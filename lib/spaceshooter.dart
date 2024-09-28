28561b57301fbd4c6c1eefdf11424f6b8176161a
alxayeed <alxayeed@gmail.com>
2024-09-28 20:12:29 +0600

    ADD: parallax data added

--- lib/spaceshooter.dart
+++ lib/spaceshooter.dart
@@ -1,7 +1,10 @@
 import 'dart:async';
 
+import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
+import 'package:flutter/cupertino.dart';
 
 import 'components/player.dart';
 
@@ -9,10 +12,19 @@ class SpaceShooter extends FlameGame with PanDetector {
   late Player player;
   @override
   Future<void> onLoad() async {
-    await super.onLoad();
+    // await super.onLoad();
+    final parallax = await loadParallaxComponent([
+      ParallaxImageData("stars_0.png"),
+      ParallaxImageData("stars_1.png"),
+      ParallaxImageData("stars_2.png"),
+    ],
+        baseVelocity: Vector2(0, 5),
+        repeat: ImageRepeat.repeat,
+        velocityMultiplierDelta: Vector2(0, -5));
+
+    add(parallax);
 
     player = Player();
-
     add(player);
   }
 
